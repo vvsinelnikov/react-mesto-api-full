b6a1000761b194c75cec96f40ec6fe0886350154
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-06-29 23:41:26 +0300

    fix: CORS experiments 03

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,7 @@ app.use(function(req, res, next) {
   if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
