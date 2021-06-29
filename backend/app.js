7a67a2107233f4f7e5af8d28103fb4a31fb62c4a
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-06-30 02:25:00 +0300

    fix: cors

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,8 @@ const allowedCors = [
 ];
 app.use(function(req, res, next) {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
+  // if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
+  if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', '*'); }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
