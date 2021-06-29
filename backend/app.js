a48e005bc960bf23ff286967970a6a1fcbe48bea
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-06-30 00:15:48 +0300

    fix: CORS experiments 08

--- backend/app.js
+++ backend/app.js
@@ -45,10 +45,10 @@ const allowedCors = [
 app.use(function(req, res, next) {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
-  res.send(origin);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.send(origin);
   next();
 });
 
