6a7a47e9fd6fe1fa726c2e5440c7098192cd42d3
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-06-30 00:00:14 +0300

    fix: CORS experiments 04

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 // разрешаем CORS
 //app.options('/signin', cors())
 //app.options('/signup', cors())
-// app.options('*', cors())
+//app.options('*', cors())
 //app.use(cors())
 const allowedCors = [
   'http://mesto-vv.nomoredomains.monster',
@@ -45,8 +45,9 @@ const allowedCors = [
 app.use(function(req, res, next) {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
+  //res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
