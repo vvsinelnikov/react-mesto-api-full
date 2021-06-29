205db3a1adeb2ffaf0b80ffa9a94f199768092b4
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-06-30 02:29:34 +0300

    fix: cors 2

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ const allowedCors = [
 app.use(function(req, res, next) {
   const { origin } = req.headers;
   // if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
-  if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', '*'); }
+  if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
