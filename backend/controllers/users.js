674e322a226d5217a0dbc4f442f43aa293e7c216
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-06-30 00:46:21 +0300

    fix: salt und

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
 const { NODE_ENV, JWT_SECRET, SALT_ROUNDS } = process.env;
-const saltRounds = NODE_ENV === 'production' ? SALT_ROUNDS : 8
+// const saltRounds = NODE_ENV === 'production' ? SALT_ROUNDS : 8;
+const saltRounds = 8;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
