976af014dbd16b7b40e6e63e8dc6e4278f8cc356
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-06-30 12:34:37 +0300

    cors 5

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,15 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 //app.use(express.static(path.join(__dirname, 'public')));
 
 // разрешаем CORS
-app.options('*', function (req,res) { res.sendStatus(200); });
+app.options('*', function (req,res) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
+  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 const allowedCors = [
   'http://mesto-vv.nomoredomains.monster',
   'https://mesto-vv.nomoredomains.monster',
